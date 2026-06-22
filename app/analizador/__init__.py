6eb5e775a5e8663b37f487ab049f121d1114bf3f
Tecla Fernandes Oliveira <115437392+TeclaFernandes@users.noreply.github.com>
2026-06-22 16:52:10 -0300

    refactor: Ajuste para import dos arquivos

---
+++ app/analizador/__init__.py
@@ -0,0 +1,10 @@
+from .caracteristicas import AnalisadorCaracteristicas
+from .motor_decisao import MotorDecisao
+from .questionario import obter_requisitos, obter_requisitos_direto
+
+__all__ = [
+    "AnalisadorCaracteristicas",
+    "MotorDecisao",
+    "obter_requisitos",
+    "obter_requisitos_direto"
+]
