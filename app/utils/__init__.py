2d53468545e48612b1a9d6bb9da2121e3c15a51a
Tecla Fernandes Oliveira <115437392+TeclaFernandes@users.noreply.github.com>
2026-06-22 17:01:37 -0300

    refactor: Ajuste para import dos arquivos

---
+++ app/utils/__init__.py
@@ -0,0 +1,11 @@
+from .gerador import gerar_aleatorio
+from .benchmark import Benchmark
+from .contador import Contador
+from .complexidade import COMPLEXIDADES
+
+__all__ = [
+    "gerar_aleatorio",
+    "Benchmark",
+    "Contador", 
+    "COMPLEXIDADES"
+]
